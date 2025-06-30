053a4b782f37102f0269cf387fc688a974e8a7ba
e314521 <99789999@qq.com>
2025-06-30 13:29:46 +0800

    Create ndk-build.sh

---
+++ ndk-build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Path to NDK (需提前设置NDK_HOME环境变量)
+NDK="$NDK_HOME"
+
+# 工具路径
+CMAKE="cmake"
+MAKE="make"
+BUILD_PATH="cmake_build"
+
+# 目标ABI
+ABIs=("arm64-v8a")
+
+for abi in "${ABIs[@]}"; do
+    echo "=========================="
+    echo "= Building $abi"
+    echo "=========================="
+
+    $CMAKE -S. -B$BUILD_PATH/$abi -G "Unix Makefiles" \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=TRUE \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_TOOLCHAIN_FILE=$NDK/build/cmake/android.toolchain.cmake \
+    -DCMAKE_C_COMPILER=$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/clang \
+    -DCMAKE_CXX_COMPILER=$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ \
+    -DANDROID_NDK=$NDK \
+    -DANDROID_ABI=$abi \
+    -DANDROID_NATIVE_API_LEVEL=21
+
+    $MAKE -C$BUILD_PATH/$abi -j16
+done
